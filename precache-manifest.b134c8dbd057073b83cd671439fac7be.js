9e9c9fa453fc03f7b45fbab29163e80db4e1b75a
santoshtalluri <santoshtalluri@gmail.com>
2022-05-06 02:12:12 -0700

    Updates

---
+++ precache-manifest.b134c8dbd057073b83cd671439fac7be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f93aecb2457b14097a",
+    "url": "/my-app/static/css/main.3269cda7.chunk.css"
+  },
+  {
+    "revision": "80f93aecb2457b14097a",
+    "url": "/my-app/static/js/main.80f93aec.chunk.js"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "5972a582c85a7ffc5352",
+    "url": "/my-app/static/js/2.5972a582.chunk.js"
+  },
+  {
+    "revision": "298cfb8198032cb8cffc56e60b78c886",
+    "url": "/my-app/index.html"
+  }
+];
